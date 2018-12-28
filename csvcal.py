55f28ebbe9f482ba5889ae63faa21d419cfcba5e
Florian Limberger <flo@snakeoilproductions.net>
2018-12-28 11:30:58 +0000

    Refactor common/ugly code into separate functions

--- csvcal.py
+++ csvcal.py
@@ -31,35 +31,41 @@ def to_csv(output_file, input_file):
 
 def create_properties_map(calendar):
     properties = {}
+    for event in iter_events(calendar):
+        for prop in iter_properties(event):
+            property_name = prop[0]
+            properties[property_name] = properties.get(property_name, 0) + 1
+    return properties
+
+
+def iter_events(calendar):
     for component in calendar.walk():
         if component.name == 'VEVENT':
-            for prop in component.property_items(recursive=False):
-                property_name = prop[0]
-                if property_name == 'BEGIN' or property_name == 'END':
-                    continue
-                if property_name in properties:
-                    properties[property_name] += 1
-                else:
-                    properties[property_name] = 1
-    return properties
+            yield component
+
+
+def iter_properties(event):
+    for prop in event.property_items(recursive=False):
+        property_name = prop[0]
+        if property_name != 'BEGIN' and property_name != 'END':
+            yield prop
 
 
 def write_events_to_csv(output_file, calendar, property_names):
     writer = CSVWriter(output_file)
     writer.writerow(property_names)
-    for component in calendar.walk():
-        if component.name == 'VEVENT':
-            props = []
-            for property_name in property_names:
-                prop = None
-                if property_name in component:
-                    obj = component[property_name]
-                    if hasattr(obj, 'to_ical'):
-                        prop = obj.to_ical().decode('UTF-8')
-                    else:
-                        prop = obj
-                props.append(prop)
-            writer.writerow(props)
+    for event in iter_events(calendar):
+        props = []
+        for property_name in property_names:
+            prop = None
+            if property_name in event:
+                obj = event[property_name]
+                if hasattr(obj, 'to_ical'):
+                    prop = obj.to_ical().decode('UTF-8')
+                else:
+                    prop = obj
+            props.append(prop)
+        writer.writerow(props)
 
 
 def to_ics(output_file, input_file):
